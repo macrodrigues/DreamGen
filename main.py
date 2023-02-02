a7d829f2c4f3e28950b41eecb30a95afa43e8e05
Marco Rodrigues <marcoacavaco.rodrigues@hotmail.com>
2023-02-01 22:24:21 -0300

    ScreenManager added

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 from kivymd.app import MDApp, Builder
-from kivymd.uix.boxlayout import MDBoxLayout
 from kivy.core.window import Window
+from kivy.uix.screenmanager import Screen, ScreenManager
 
 # --- this changes the app's default background --- #
 Window.clearcolor = (.9, .9, .9, 1)
 Window.size = (400, 500)
 
-class Intro(MDBoxLayout):
+class HomeScreen(Screen):
     pass
 
-class MyHome(MDBoxLayout):
-    pass
+class ResultsScreen(Screen):
+    print('all good')
 
-class MyApp(MDApp):
-    pass
+class Main(MDApp):
+    def build(self):
+        Builder.load_file("layout.kv")
+        sm = ScreenManager()
+        sm.add_widget(HomeScreen(name='home'))
+        sm.add_widget(ResultsScreen(name='results'))
+        return sm
 
-MyApp().run()
+Main().run()
